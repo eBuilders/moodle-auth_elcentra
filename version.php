51577eb85c2f12faa223d749adc36020c407fd41
Skandha Prasad <sskandhaprasad@gmail.com>
2013-11-22 00:56:10 +0530

    Update on version.php

--- version.php
+++ version.php
@@ -2,10 +2,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013102800;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2013112200;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2011120500;        // Requires this Moodle version
 $plugin->component = 'auth_elcentra';       // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_BETA;
 $plugin->cron      = 0;
 $plugin->dependencies = array();
-$plugin->release = '1.0'; 
+$plugin->release = '1.02'; 
